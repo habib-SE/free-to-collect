7b9eb7bf0eda17029339053b0de5b65879650304
habib-SE <hab67603@gmail.com>
2023-12-08 14:47:50 +0500

    create a full stucture of whole project

--- charity-free-to-collect/src/index.js
+++ charity-free-to-collect/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { FirebaseProvider } from './context/Firebase';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FirebaseProvider>
     <App />
+    </FirebaseProvider>
   </React.StrictMode>
 );
